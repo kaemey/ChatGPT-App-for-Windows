b7a45a49528205aa5cf3e810551c1f4213f31441
Максим <kaemey@gmail.com>
2023-11-29 09:34:12 +0300

    Context

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ eel.init('templates')
 import g4f
 
 g4f.check_version = False  # Disable automatic version checking
+context = list()
 
 @eel.expose
 def getResponse(content):
+    context.append({"role": "user", "content": content})
     response = g4f.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": content}],
+        messages=context,
     )  # Alternative model setting
+    context.append({"role": "assistant", "content": response})
     return response
 
 @eel.expose
